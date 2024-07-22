f357e8d974c647aac21b688b7eb6e1c1ede4754c
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2024-07-22 21:47:55 +0300

    fix:исправляем хук useLocalStorage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useLocalStorage } from './hooks/useLocalStorage'
 import './App.css'
 
 function App() {
-    const [token, { setItem, removeItem }] = useLocalStorage('token')
+    const { token, ls } = useLocalStorage('token')
     return (
         <div className='App'>
             <header className='App-header'>
                 <div>
                     <p>Твой токен: {token}</p>
                     <div>
-                        <button onClick={() => setItem('new-token')}>Задать токен</button>
-                        <button onClick={() => removeItem()}>Удалить токен</button>
+                        <button onClick={() => ls.setItem('new-token')}>
+                            Задать токен
+                        </button>
+                        <button onClick={() => ls.removeItem()}>Удалить токен</button>
                     </div>
                 </div>
             </header>
